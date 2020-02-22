1614be3485f04e0a5c77780af52ae8d7d6822815
dhruva darshan b s <46771469+dhruva7676@users.noreply.github.com>
2020-02-22 12:12:45 +0530

    Update UnitTest1.cs

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -10,7 +10,7 @@ using Assert = NUnit.Framework.Assert;
 using System.Text.RegularExpressions;
 using System.Linq;
 using System.Threading;
-
+    using other = sssssss.a;
 namespace PAIS
 {
     [TestFixture]
@@ -2197,4 +2197,28 @@ namespace PAIS
             driver.Close();
         }
     }
+
+namespace ConsoleApplication1
+{
+    public class a 
+    {
+        public string ff { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            other s = new other();
+            a b = new a();
+        }
+    }
+}
+namespace sssssss 
+{
+
+    public class a
+    {
+        public string ff { get; set; }
+    }
+}
 }
